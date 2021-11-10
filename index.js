25838e62e072453492d6b2e60a42bf20d7769c04
Carlotta Chen <carlottachen92@gmail.com>
2021-11-10 15:11:47 -0800

    Working addition

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let jamieLannisterHealth = 50;
 let jonSnowDefense = 0;
 let jamieLannisterDefense = 20;
 
+/*
 if(jonSnowAttack < jamieLannisterAttack){
 	console.log("Jamie has the stronger attack!");
 }else if(jonSnowAttack > jamieLannisterAttack){
@@ -14,16 +15,27 @@ if(jonSnowAttack < jamieLannisterAttack){
 }else{
 	console.log("The attacks are equally powerful.");
 }
+*/
 
 jonSnowDefense += 25;
 
+/*
 if(jonSnowHealth <= 0){
 	console.log("Jon Snow has perished in battle");
 }else{
-	jonSnowHealth -= jamieLannisterAttack;
+	jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense);
 //use ` backtick for ${}	
 	console.log(`Jon's health is ${jonSnowHealth}`);
 }
+*/
 
+for(let i = 0; i < 3; i++){
+	if(jonSnowHealth <= 0){
+		console.log("Jon Snow has perished in battle");
+	}else{
+		jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense);
+		console.log(`Jon's health is ${jonSnowHealth}`);
+	}
+}
 
 
