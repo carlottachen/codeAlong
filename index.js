b370bbecb6f70bb1bb6595fce95ffbd6d42b8639
Carlotta Chen <carlottachen92@gmail.com>
2021-11-10 11:24:13 -0800

    Second implementation working

--- index.js
+++ index.js
@@ -2,11 +2,28 @@
 
 let jonSnowAttack = 25;
 let jamieLannisterAttack = 35;
+let jonSnowHealth = 50;
+let jamieLannisterHealth = 50;
+let jonSnowDefense = 0;
+let jamieLannisterDefense = 20;
 
 if(jonSnowAttack < jamieLannisterAttack){
-	console.log('Jamie has the stronger attack!'));
+	console.log("Jamie has the stronger attack!");
+}else if(jonSnowAttack > jamieLannisterAttack){
+	console.log("Jon has the stronger attack!");
 }else{
-	console.log('Jon has the stronger attack!'));
+	console.log("The attacks are equally powerful.");
 }
+
+jonSnowDefense += 25;
+
+if(jonSnowHealth <= 0){
+	console.log("Jon Snow has perished in battle");
+}else{
+	jonSnowHealth -= jamieLannisterAttack;
+//use ` backtick for ${}	
+	console.log(`Jon's health is ${jonSnowHealth}`);
+}
+
 
 
