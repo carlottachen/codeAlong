380f0a1fb2dabe42ed9f34afca7889375334a150
Carlotta Chen <carlottachen92@gmail.com>
2021-11-10 11:08:52 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log("It's working!");
+
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+
+if(jonSnowAttack < jamieLannisterAttack){
+	console.log('Jamie has the stronger attack!'));
+}else{
+	console.log('Jon has the stronger attack!'));
+}
+
+
